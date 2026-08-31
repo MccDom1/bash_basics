19d89f175f71ed14291be3d2e87153104d664393
Dominique McClaney <mcclaneydominique@gmail.com>
2026-08-30 20:46:10 -0400

    Add alternate morning logic on parallel branch

--- 04-life.sh
+++ 04-life.sh
@@ -9,6 +9,15 @@ else
    echo "Awww... You don't know the meaning of life"
 fi
 
+echo "Enter the current hour from 0-23:"
+read hour
+
+if [ "$hour" -ge 6 ] && [ "$hour" -lt 12 ]; then
+    echo "Yes, it is morning!"
+else
+    echo "No, it is not morning."
+fi
+
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
